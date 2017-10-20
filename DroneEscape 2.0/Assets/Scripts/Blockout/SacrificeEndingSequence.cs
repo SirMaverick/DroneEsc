d9ca317efb6036c823037c4488ac4904ddea3944
HKUstudentje <tbenjaminh@gmail.com>
2017-10-20 12:54:59 +0200

    Finished cutscene's and both choice endings in the blockout. In a very rough state.

---
+++ DroneEscape 2.0/Assets/Scripts/Blockout/SacrificeEndingSequence.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SacrificeEndingSequence : MonoBehaviour
+{
+
+    [SerializeField] private Camera playerCamera, camera1, camera2, camera3;
+    [SerializeField] private float timeBetweenCamera;
+    [SerializeField] private float timeForCore;
+    [SerializeField] private GameObject core;
+    [SerializeField] private GameObject spawner1, spawner2, spawner3;
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        camera1.enabled = false;
+        camera2.enabled = false;
+        camera3.enabled = false;
+        spawner1.SetActive(false);
+        spawner2.SetActive(false);
+        spawner3.SetActive(false);
+        core.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Drone")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                print("E has been pressed");
+                StartCoroutine(sacrificeSequence());
+            }
+        }
+    }
+
+    IEnumerator sacrificeSequence()
+    {
+        core.SetActive(true);
+        yield return new WaitForSeconds(timeForCore);
+        playerCamera.enabled = false;
+        spawner1.SetActive(true);
+        camera1.enabled = true;
+        yield return new WaitForSeconds(timeBetweenCamera);
+        camera1.enabled = false;
+        spawner1.SetActive(false);
+        spawner2.SetActive(true);
+        camera2.enabled = true;
+        yield return new WaitForSeconds(timeBetweenCamera);
+        camera2.enabled = false;
+        spawner2.SetActive(false);
+        spawner3.SetActive(true);
+        camera3.enabled = true;
+        yield return new WaitForSeconds(timeBetweenCamera);
+        FadeToWhite.Instance.CallFading();
+    }
+}
