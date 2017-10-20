e9955f36138e38ddc1a1a38e912da0abd9b86fb4
SirMaverick <rickruiten@hotmail.com>
2017-10-20 14:51:09 +0200

    Added magnet scripts + fixed conveyer belt bug

---
+++ DroneEscape 2.0/Assets/Scripts/Prototype2.0/MagnetButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetButton : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject magnet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DroneEscape 2.0/Assets/Scripts/Prototype2.0/MagnetMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetMove : MonoBehaviour {
+
+    public bool turnedOn;
+    [SerializeField] private float speed = 2.5f;
+    bool turnedOff;
+    bool on;
+    List<GameObject> listOfDrones = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(turnedOff) {
+            ReleaseDrones();
+        }
+        foreach (GameObject drone in listOfDrones) {
+            if(drone.transform.position.y < transform.position.y - transform.localScale.y / 2 - 0.7f)
+            drone.transform.Translate(0, Time.deltaTime * speed, 0, Space.World);
+        }
+
+    }
+
+    void ReleaseDrones() {
+        turnedOff = false;
+        foreach (Transform child in transform) {
+            child.transform.parent = null;
+            child.GetComponent<Rigidbody>().useGravity = true;
+        }
+        listOfDrones.Clear();
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.tag == "Drone" && on && other.GetComponent<Rigidbody>().useGravity) {
+            if (other.GetComponent<Rigidbody>().useGravity) {
+                other.transform.parent = transform;
+                other.GetComponent<Rigidbody>().useGravity = false;
+                listOfDrones.Add(other.gameObject);
+            }
+
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(turnedOn) {
+            if(other.transform.tag == "Drone" && other.transform.parent != gameObject) {
+                other.transform.parent = transform;
+                other.GetComponent<Rigidbody>().useGravity = false;
+                listOfDrones.Add(other.gameObject);
+            }
+            turnedOn = false;
+            on = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (on) {
+            ReleaseDrones();
+        }
+        
+    }
+}
