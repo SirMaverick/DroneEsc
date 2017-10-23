64dc78df8425ba97290fa0668e8a99e1c86c6e4c
HKUstudentje <tbenjaminh@gmail.com>
2017-10-23 14:26:51 +0200

    Made a prototype (in blockout) of how the antagonist arm could grab the player if caught.

---
+++ DroneEscape 2.0/Assets/Scripts/Blockout/AntagonistArmMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntagonistArmMover : MonoBehaviour
+{
+    [SerializeField] public float moveDistance;
+    private Vector3 startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(startPos, transform.position) <= moveDistance)
+        {
+            this.transform.Translate(Vector3.down * 0.5f, Space.World);
+        }
+    }
+}
