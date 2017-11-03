193d812e25e8dc1e1ea7a400f19cb7414666e3a9
jeroenhuisen <jeroenhuisen@outlook.com>
2017-11-03 14:34:36 +0100

    Magnet movement

--- DroneEscape 2.0/Assets/Scripts/MovementControllers/MagnetMovementController.cs
+++ DroneEscape 2.0/Assets/Scripts/MovementControllers/MagnetMovementController.cs
@@ -3,22 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 public class MagnetMovementController : MovementController
 {
-   
+    [SerializeField]
+    private GameObject magnet;
+    public Camera surveillanceCamera;
+    public float speed = 2;
+    public bool coreInside;
+    public GameObject drone;
 
-    public override void Horizontal(float speed)
+    public override void Horizontal(float direction)
     {
-
+        magnet.transform.Translate(direction * Time.deltaTime * speed, 0, 0);
     }
 
     public override void Vertical(float direction)
     {
-       // Do nothing
+        magnet.transform.Translate(0, 0, direction * Time.deltaTime * speed);
     }
 
     public override void Look(Vector2 md)
     {
         // Do nothing
     }
+
+    public override void RightClick(bool key)
+    {
+        if (key)
+        {
+            magnet.GetComponent<MagnetMove>().turnedOn = true;
+        }
+        else
+        {
+            magnet.GetComponent<MagnetMove>().turnedOn = false;
+        }
+
+    }
+
 
     public override void Use(bool key)
     {
