75d4540ee544712438d8cd507b965b24deada011
HKUstudentje <tbenjaminh@gmail.com>
2017-11-21 10:21:31 +0100

    Made building block prefabs, began putting them into the blockout.

--- DroneEscape 2.0/Assets/Scripts/Blockout/DoorOpener.cs
+++ DroneEscape 2.0/Assets/Scripts/Blockout/DoorOpener.cs
@@ -10,16 +10,12 @@ public class DoorOpener : MonoBehaviour {
     private Animator _animatorR1;
     private Animator _animatorL2;
     private Animator _animatorR2;
-    private Animator _animatorL3;
-    private Animator _animatorR3;
     [SerializeField] private GameObject DoorL;
     [SerializeField] private GameObject DoorR;
     [SerializeField] private GameObject DoorL1;
     [SerializeField] private GameObject DoorR1;
     [SerializeField] private GameObject DoorL2;
     [SerializeField] private GameObject DoorR2;
-    [SerializeField] private GameObject DoorL3;
-    [SerializeField] private GameObject DoorR3;
 
     // Use this for initialization
     void Start () {
@@ -29,8 +25,6 @@ public class DoorOpener : MonoBehaviour {
         _animatorR1 = DoorR1.GetComponent<Animator>();
         _animatorL2 = DoorL2.GetComponent<Animator>();
         _animatorR2 = DoorR2.GetComponent<Animator>();
-        _animatorL3 = DoorL3.GetComponent<Animator>();
-        _animatorR3 = DoorR3.GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -43,8 +37,6 @@ public class DoorOpener : MonoBehaviour {
             _animatorR1.SetBool("Open", true);
             _animatorL2.SetBool("Open", true);
             _animatorR2.SetBool("Open", true);
-            _animatorL3.SetBool("Open", true);
-            _animatorR3.SetBool("Open", true);
         }    
     }
 }
