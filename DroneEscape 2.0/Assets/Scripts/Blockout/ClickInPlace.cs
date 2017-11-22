830eadd63f8faddb935693f9a106afe563659b2b
HKUstudentje <tbenjaminh@gmail.com>
2017-11-22 15:25:26 +0100

    Working on last puzzle, adding prefabs amd models

---
+++ DroneEscape 2.0/Assets/Scripts/Blockout/ClickInPlace.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickInPlace : MonoBehaviour {
+
+    [SerializeField] private Transform triggerObjectT;
+    private Transform triggerT;
+    public bool hasBeenSet;
+
+    private void Start()
+    {
+        hasBeenSet = false;
+        triggerT = gameObject.transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Magnetic" && hasBeenSet == false)
+        {
+            other.transform.position = transform.parent.position;
+            hasBeenSet = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Magnetic" && hasBeenSet == true)
+        {
+            hasBeenSet = false;
+        }
+    }
+}
