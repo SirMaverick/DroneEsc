4396c708831e8fd8fad3539acecd176b56d5c3f8
jeroenhuisen <jeroenhuisen@outlook.com>
2017-12-04 11:43:03 +0100

    DroneArmTrigger added

---
+++ DroneEscape 2.0/Assets/Scripts/ArmTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmTrigger : MonoBehaviour
+{
+    private SystemArm systemArm;
+    [SerializeField] private GameObject droneObject;
+    [SerializeField] private bool alwaysTriggerOnEnter = false;
+    private bool alreadyTriggered = false;
+
+    private void Start()
+    {
+        systemArm = FindObjectOfType<SystemArm>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (alwaysTriggerOnEnter)
+        {
+            systemArm.MoveTo(droneObject);
+        }
+        else
+        {
+            if (!alreadyTriggered)
+            {
+                systemArm.MoveTo(droneObject);
+                alreadyTriggered = true;
+            }
+        }
+
+    }
+}
