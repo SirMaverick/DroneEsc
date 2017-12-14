a0bb51e44cedb4c5ae6984749094eec036454d56
SirMaverick <rickruiten@hotmail.com>
2017-12-14 15:57:34 +0100

    added door sounds

--- DroneEscape 2.0/Assets/Scripts/Blockout/DoorOpener.cs
+++ DroneEscape 2.0/Assets/Scripts/Blockout/DoorOpener.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class DoorOpener : MonoBehaviour {
 
@@ -8,8 +9,20 @@ public class DoorOpener : MonoBehaviour {
     [SerializeField] private float timeBeforeClose;
     [SerializeField] private GameObject Door;
 
+    private FMOD.Studio.EventDescription description;
+
+    private FMOD.Studio.EventInstance doorSound;
+    private FMOD.Studio.PLAYBACK_STATE playback;
+    private int audioLength;
+    bool startSound, stopSound;
+        
     // Use this for initialization
     void Start () {
+        doorSound = RuntimeManager.CreateInstance("event:/SFX/Door/DoorOpenAndClose");
+        RuntimeManager.AttachInstanceToGameObject(doorSound, transform, GetComponent<Rigidbody>());
+
+        doorSound.getDescription(out description);
+        description.getLength(out audioLength);
         _animator = Door.GetComponent<Animator>();
         _animator.SetBool("openDoor", false);
     }
@@ -19,17 +32,44 @@ public class DoorOpener : MonoBehaviour {
         if(other.tag == "Drone" || other.tag == "NPCDrone")
         {
             _animator.SetBool("openDoor", true);
+            StartCoroutine(OpenDoor(audioLength));
+
         }
+    }
+
+    private void Update() {
+        doorSound.getPlaybackState(out playback);
+        print(audioLength);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        StartCoroutine(DoorCloser());
+        if (other.tag == "Drone" || other.tag == "NPCDrone") {
+            StartCoroutine(DoorCloser(audioLength));
+        }
+         
     }
 
-    IEnumerator DoorCloser()
+    IEnumerator DoorCloser(float audioTime)
     {
+
         yield return new WaitForSeconds(timeBeforeClose);
         _animator.SetBool("openDoor", false);
+        RuntimeManager.AttachInstanceToGameObject(doorSound, transform, GetComponent<Rigidbody>());
+        doorSound.setParameterValue("MainDoorClose", 1.0f);
+        doorSound.setParameterValue("MainDoorOpen", 0.0f);
+        doorSound.start();
+        yield return new WaitForSeconds(audioTime / 1000.0f);
+        doorSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+
+    }
+
+    IEnumerator OpenDoor(float audioTime) {
+        RuntimeManager.AttachInstanceToGameObject(doorSound, transform, GetComponent<Rigidbody>());
+        doorSound.setParameterValue("MainDoorClose", 0.0f);
+        doorSound.setParameterValue("MainDoorOpen", 1.0f);
+        doorSound.start();
+        yield return new WaitForSeconds(audioTime / 1000.0f);
+        doorSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
     }
 }
