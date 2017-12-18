b351fe8b025de3e6d01837ffa8f12af512b9d452
HKUstudentje <tbenjaminh@gmail.com>
2017-12-18 19:14:46 +0100

    Commit tried timing machines... it sucks... love it.

---
+++ DroneEscape 2.0/Assets/Scripts/Blockout/BasicTimerAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTimerAnimation : MonoBehaviour {
+
+    private Animator _animator;
+    [SerializeField] private float timeBeforeClose;
+    [SerializeField] private float timeBeforeOpen;
+    [SerializeField] private float firstTimeClose;
+    [SerializeField] private GameObject animObject;
+    public bool Close;
+
+    // Use this for initialization
+    void Start () {
+
+        _animator = animObject.GetComponent<Animator>();
+        _animator.SetBool("Close", false);
+        StartCoroutine(CloseAnim(firstTimeClose));
+    }
+
+    IEnumerator CloseAnim(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _animator.SetBool("Close", true);
+        StartCoroutine(OpenAnim(timeBeforeOpen));
+    }
+    IEnumerator OpenAnim(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _animator.SetBool("Close", false);
+        StartCoroutine(CloseAnim(timeBeforeClose));
+    }
+
+}
