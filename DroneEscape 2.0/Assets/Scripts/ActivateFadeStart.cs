2128faba213a887660c5a36d436b8c6561ffe23e
jeroenhuisen <jeroenhuisen@outlook.com>
2017-12-19 13:46:21 +0100

    New stuff credits

---
+++ DroneEscape 2.0/Assets/Scripts/ActivateFadeStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateFadeStart : MonoBehaviour
+{
+
+    [SerializeField] private float timeBeforeFade;
+
+    private void Start()
+    {
+        StartCoroutine(fadeToWhite());
+    }
+
+    IEnumerator fadeToWhite()
+    {
+        yield return new WaitForSeconds(timeBeforeFade);
+        FadeToWhite.Instance.CallFading();
+    }
+}
