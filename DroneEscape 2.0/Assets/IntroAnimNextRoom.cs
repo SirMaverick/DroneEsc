12c9a8ae4c89e7736631e60ce355853ea22d843f
jeroenhuisen <jeroenhuisen@outlook.com>
2018-01-22 17:22:20 +0100

    Animations for intro scene

---
+++ DroneEscape 2.0/Assets/IntroAnimNextRoom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAnimNextRoom : MonoBehaviour {
+
+    [SerializeField] private IntroAnimation[] drones;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Drone")
+        {
+            foreach (IntroAnimation drone in drones)
+            {
+                drone.ThrowAllowed();
+            }
+        }
+    }
+}
---
+++ DroneEscape 2.0/Assets/Scripts/Animations/IntroTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class IntroTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "NPCDrone")
+        {
+            other.gameObject.GetComponent<Animator>().SetBool("PickUpCore", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "NPCDrone")
+        {
+            other.gameObject.GetComponent<Animator>().SetBool("PickUpCore", false);
+            other.gameObject.GetComponent<IntroAnimation>().Pause();
+        }
+    }
+}
+
